d82deabc51c2fd7962d9623e3111e2c54f20cb99
shivcodecf <shivam2113128@akgec.ac.in>
2025-09-02 04:02:08 +0530

    all required functions working

--- lib/chat_server_web/channels/chat_channel.ex
+++ lib/chat_server_web/channels/chat_channel.ex
@@ -1,19 +1,39 @@
 defmodule ChatServerWeb.ChatChannel do
   use Phoenix.Channel
+  alias ChatServerWeb.Presence
 
   def join("chat:" <> chat_id, _params, socket) do
     user_id = socket.assigns[:user_id]
 
-    # Ensure membership exists (from earlier code)
+    # Ensure membership exists
     ensure_membership(chat_id, user_id)
 
-    {:ok, socket}
+    # Track presence after join
+    send(self(), :after_join)
+
+    {:ok, assign(socket, :chat_id, chat_id)}
+  end
+
+  # Handle presence after join
+  def handle_info(:after_join, socket) do
+    user_id = socket.assigns[:user_id]
+
+    # Track this user in Presence
+    {:ok, _} =
+      Presence.track(socket, user_id, %{
+        online_at: System.system_time(:second)
+      })
+
+    # Push the full presence state to this user
+    push(socket, "presence_state", Presence.list(socket))
+
+    {:noreply, socket}
   end
 
   # 👇 Handle incoming messages
   def handle_in("new_message", %{"content" => content}, socket) do
     user_id = socket.assigns[:user_id]
-    chat_id = String.replace_prefix(socket.topic, "chat:", "")
+    chat_id = socket.assigns[:chat_id]
 
     # Build message
     msg = %{
@@ -26,7 +46,7 @@ defmodule ChatServerWeb.ChatChannel do
     # Broadcast to everyone in the room
     broadcast!(socket, "new_message", msg)
 
-    # TODO: also persist in Supabase
+    # Persist in Supabase
     persist_message(msg)
 
     {:noreply, socket}
@@ -47,7 +67,6 @@ defmodule ChatServerWeb.ChatChannel do
     |> Finch.request(ChatServerFinch)
   end
 
-  # 👇 your ensure_membership from earlier
   defp ensure_membership(chat_id, user_id) do
     url = Application.fetch_env!(:chat_server, :supabase_url) <> "/rest/v1/chat_members"
 
